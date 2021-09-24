227d0e687abe380f77ad0b52a9c72dc00f408920
Matthias R. Wiora <matthias@wiora.io>
2021-09-24 14:49:59 +0200

    defect(asn1): keyUsage not built

--- test/fixtures/gen-certs.js
+++ test/fixtures/gen-certs.js
@@ -1,15 +1,57 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var fixtures = require('../fixtures');
+var fixtures = require('.');
+var rfc2560 = require('asn1.js-rfc2560');
+var rfc5280 = require('asn1.js-rfc5280');
+
+var crypto = require('crypto');
+
+var OCSPEndPoint = 'http://127.0.0.1:8000/ocsp';
+
+console.debug("getOCSPCert setAIA")
+var ext = rfc5280.AuthorityInfoAccessSyntax.encode([ {
+  accessMethod: rfc2560['id-pkix-ocsp'],
+  accessLocation: {
+    type: 'uniformResourceIdentifier',
+    value: OCSPEndPoint
+  }
+} ], 'der');
+
+console.debug("BEFORE")
+// var keyUsage = rfc5280.KeyUsage.encode(1, 'der');
+
+
+// const bitArray = new ArrayBuffer(1);
+// const bitView = new Uint8Array(bitArray);
+
+// bitView[0] |= 0x02; // Key usage "cRLSign" flag
+// bitView[0] |= 0x04; // Key usage "keyCertSign" flag
+
+// console.debug("BEFORE")
+// var key = rfc5280.KeyUsage.encode('00000001', 'der');
+// console.log(key)
+console.debug("AFTER")
 
 var options = {
   serial: 42,
   commonName: 'mega.ca',
-  size: 2048
+  size: 2048,
+  extensions: [ {
+    extnID: [1, 3, 6, 1, 5, 5, 7, 1, 1],
+    critical: false,
+    extnValue: ext
+  },
+  {
+    extnID: [2, 5, 29, 15],
+    critical: true,
+    extnValue: 1
+  }
+]
 };
 
 fixtures.getOCSPCert(options, function(cert, key) {
+  console.debug("gen issuer")
   fs.writeFileSync(__dirname + '/issuer-cert.pem', cert);
   fs.writeFileSync(__dirname + '/issuer-key.pem', key);
 
@@ -17,7 +59,18 @@ fixtures.getOCSPCert(options, function(cert, key) {
     issuer: cert,
     issuerKey: key,
     serial: 43,
-    size: 2048
+    size: 2048,
+    extensions: [ {
+      extnID: [1, 3, 6, 1, 5, 5, 7, 1, 1],
+      critical: false,
+      extnValue: ext
+    },
+    {
+      extnID: [2, 5, 29, 15],
+      critical: true,
+      extnValue: 'digitalSignature'
+    }
+  ]
   };
 
   fixtures.getOCSPCert(options, function(cert, key) {
